ed7ca50e388a8fc5b5d168cf684d5dde87c088e2
rodking <alldnnwanglu@users.noreply.github.com>
2017-10-13 18:18:52 +0800

    java ArryList 部分注释！

--- src/main/jdk8/java/util/ArrayList.java
+++ src/main/jdk8/java/util/ArrayList.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Copyright (c) 1997, 2013, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  *
@@ -101,6 +101,7 @@ import java.util.function.UnaryOperator;
  * @see     LinkedList
  * @see     Vector
  * @since   1.2
+ *
  */
 
 public class ArrayList<E> extends AbstractList<E>
@@ -109,12 +110,14 @@ public class ArrayList<E> extends AbstractList<E>
     private static final long serialVersionUID = 8683452581122892189L;
 
     /**
-     * Default initial capacity.
+     * Default initial capacity. <br/>
+     * 默认的初始容量
      */
     private static final int DEFAULT_CAPACITY = 10;
 
     /**
-     * Shared empty array instance used for empty instances.
+     * Shared empty array instance used for empty instances.<br/>
+     * 共享空数组实例用于空实例
      */
     private static final Object[] EMPTY_ELEMENTDATA = {};
 
@@ -122,6 +125,7 @@ public class ArrayList<E> extends AbstractList<E>
      * Shared empty array instance used for default sized empty instances. We
      * distinguish this from EMPTY_ELEMENTDATA to know how much to inflate when
      * first element is added.
+     *
      */
     private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
 
@@ -130,36 +134,40 @@ public class ArrayList<E> extends AbstractList<E>
      * The capacity of the ArrayList is the length of this array buffer. Any
      * empty ArrayList with elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA
      * will be expanded to DEFAULT_CAPACITY when the first element is added.
+     *
+     * 这个数组用来存储数据的缓存.长度为 capacity. 所有空ArryList 返回 DEFAULTCAPACITY_EMPTY_ELEMENTDATA，
+     * 当插入新元素 DEFAULT_CAPACITY 的值就会是 DEFAULT_CAPACITY
      */
     transient Object[] elementData; // non-private to simplify nested class access
 
     /**
-     * The size of the ArrayList (the number of elements it contains).
-     *
+     * The size of the ArrayList (the number of elements it contains).<br/>
+     * 在ArrayList的大小（对它包含的元素数）。
      * @serial
      */
     private int size;
 
     /**
-     * Constructs an empty list with the specified initial capacity.
+     * Constructs an empty list with the specified initial capacity. 构造一个带默认大小的空ArryList数组
      *
-     * @param  initialCapacity  the initial capacity of the list
+     * @param  initialCapacity  the initial capacity of the list 这个数组的初始容量大小
      * @throws IllegalArgumentException if the specified initial capacity
-     *         is negative
+     *         is negative. 如果指定的容量为负数抛出这个异常 IllegalArgumentException
      */
     public ArrayList(int initialCapacity) {
-        if (initialCapacity > 0) {
+        if (initialCapacity > 0) { // 初始容量大于 0 初始数组
             this.elementData = new Object[initialCapacity];
         } else if (initialCapacity == 0) {
-            this.elementData = EMPTY_ELEMENTDATA;
+            this.elementData = EMPTY_ELEMENTDATA; // ==0 返回共享默认空数组
         } else {
             throw new IllegalArgumentException("Illegal Capacity: "+
-                                               initialCapacity);
+                                               initialCapacity);  // 初始容量小于0 抛出异常 IllegalArgumentException
         }
     }
 
     /**
-     * Constructs an empty list with an initial capacity of ten.
+     * Constructs an empty list with an initial capacity of ten.<br/>
+     * 构造一个初始容量为10 的空list
      */
     public ArrayList() {
         this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
@@ -170,18 +178,20 @@ public class ArrayList<E> extends AbstractList<E>
      * collection, in the order they are returned by the collection's
      * iterator.
      *
-     * @param c the collection whose elements are to be placed into this list
-     * @throws NullPointerException if the specified collection is null
+     * 构造一个list包含指定元素，返回顺序迭代器
+     *
+     * @param c the collection whose elements are to be placed into this list 将要放入 list 的 集合
+     * @throws NullPointerException if the specified collection is null 这个特殊的集合是空
      */
     public ArrayList(Collection<? extends E> c) {
-        elementData = c.toArray();
-        if ((size = elementData.length) != 0) {
+        elementData = c.toArray(); // 将集合转换为 list
+        if ((size = elementData.length) != 0) { // 如果集合不为 0
             // c.toArray might (incorrectly) not return Object[] (see 6260652)
-            if (elementData.getClass() != Object[].class)
+            if (elementData.getClass() != Object[].class) // 如果是值类型的需要拷贝数据
                 elementData = Arrays.copyOf(elementData, size, Object[].class);
         } else {
             // replace with empty array.
-            this.elementData = EMPTY_ELEMENTDATA;
+            this.elementData = EMPTY_ELEMENTDATA; // 为空返回 空数组
         }
     }
 
@@ -189,6 +199,7 @@ public class ArrayList<E> extends AbstractList<E>
      * Trims the capacity of this <tt>ArrayList</tt> instance to be the
      * list's current size.  An application can use this operation to minimize
      * the storage of an <tt>ArrayList</tt> instance.
+     *
      */
     public void trimToSize() {
         modCount++;
@@ -204,7 +215,7 @@ public class ArrayList<E> extends AbstractList<E>
      * necessary, to ensure that it can hold at least the number of elements
      * specified by the minimum capacity argument.
      *
-     * @param   minCapacity   the desired minimum capacity
+     * @param   minCapacity   the desired minimum capacity 期望的最小容量
      */
     public void ensureCapacity(int minCapacity) {
         int minExpand = (elementData != DEFAULTCAPACITY_EMPTY_ELEMENTDATA)
